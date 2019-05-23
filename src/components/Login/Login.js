33bc34e4af9e6ed87705762394791ca34def38de
Melissa Price <melissaashprice@gmail.com>
2019-05-23 16:37:55 -0400

    login is working now :)

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -12,9 +12,9 @@ class Login extends Component {
                 <form className="form-container">
                     <h1 className="login-title">Login to your account</h1>
                     <label className="login-label">Email</label>
-                    <input name="email" className="login-input" type="text" />
+                    <input name="email" className="login-input" type="text" onChange={this.props.handleInput}/>
                     <label className="login-label">Password</label>
-                    <input className="login-input" type="password" name="password" />
+                    <input className="login-input" type="password" name="password" onChange={this.props.handleInput}/>
                     <input type='submit' value='Login' id="login-button" onClick={this.props.handleLogin}/>
                     <Link className="login-link" to="/signup">Don't have an account? Sign up!</Link>
                 </form>
