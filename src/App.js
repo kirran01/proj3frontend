0060b5bb3746da89dcc9ed982eb405395367b614
frederick <frederickbanks@icloud.com>
2019-05-23 11:14:23 -0400

    moved routes

--- src/App.js
+++ src/App.js
@@ -1,32 +1,44 @@
-import React, { Component } from 'react'
-import './App.css'
-import { Layout, Header, Navigation, Content } from 'react-mdl'
-import Main from './components/main'
-import { Link } from 'react-router-dom'
-
+import React, { Component } from "react";
+import "./App.css";
+import { Layout, Header, Navigation, Content } from "react-mdl";
+import Main from "./components/main";
+import { Link, Route } from "react-router-dom";
+import Login from './components/Login/Login'
+import Signup from './components/Signup/Signup'
 
 class App extends Component {
   render() {
     return (
       <div className="demo-big-content">
         <Layout>
-          <Header className="header-color" title={<Link id="home-link" to="/">D R O P</Link>} scroll>
-
+          <Header
+            className="header-color"
+            title={
+              <Link id="home-link" to="/">
+                D R O P
+              </Link>
+            }
+            scroll
+          >
             <Navigation>
               <Link to="/favorites">Favorited</Link>
               <Link to="/news">News</Link>
-              <Link className='login-btn' to="/login">Login</Link>
+              <Link className="login-btn" to="/login">
+                Login
+              </Link>
               <Link to="/signup">Signup</Link>
             </Navigation>
           </Header>
           <Content>
             <div className="page-content" />
             <Main />
+            <Route path="/login" component={Login} />
+            <Route path="/signup" component={Signup} />
           </Content>
         </Layout>
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
--- src/components/main.js
+++ src/components/main.js
@@ -1,19 +1,16 @@
-import React from 'react';
-import LandingPage from './Landingpage/Landingpage';
-import News from './News/news';
-import Favorites from './Favorites/Favorites'
-import Login from './Login/Login'
-import Signup from './Signup/Signup'
-import { Switch, Route } from 'react-router-dom'
+import React from "react";
+import LandingPage from "./Landingpage/Landingpage";
+import News from "./News/news";
+import Favorites from "./Favorites/Favorites";
+
+import { Switch, Route } from "react-router-dom";
 
 const Main = () => (
-    <Switch>
-        <Route exact path="/" component={LandingPage} />
-        <Route path="/news" component={News} />
-        <Route path="/favorites" component={Favorites} />
-        <Route path="/login" component={Login}/>
-        <Route path="/signup" component={Signup} />
-    </Switch>
-)
+  <Switch>
+    <Route exact path="/" component={LandingPage} />
+    <Route path="/news" component={News} />
+    <Route path="/favorites" component={Favorites} />
+  </Switch>
+);
 
 export default Main;
